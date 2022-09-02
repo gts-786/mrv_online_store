19bb1fdd22626b0e8db5545183eb92f9ea69dc3a
Roman Markovich <gts-786@mail.ru>
2022-09-02 17:10:55 +0300

    Add 7 models in file models.js

--- server/models/models.js
+++ server/models/models.js
@@ -11,3 +11,36 @@ const User = sequelize.define( 'user', {
 const Basket = sequelize.define( 'basket', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
 })
+
+const BasketDevice = sequelize.define( 'basket_device', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+const Device = sequelize.define( 'device', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+    price: {type: DataTypes.INTEGER, allowNull: false},
+    rating: {type: DataTypes.INTEGER, defaultValue: 0},
+    img: {type: DataTypes.STRING, allowNull: false}
+})
+
+const DeviceInfo = sequelize.define( 'device_info', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    title: {type: DataTypes.STRING, allowNull: false},
+    description: {type: DataTypes.STRING, allowNull: false},
+})
+
+const Brand = sequelize.define( 'brand', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+})
+
+const Type = sequelize.define( 'type', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    name: {type: DataTypes.STRING, unique: true, allowNull: false},
+})
+
+const Rating = sequelize.define( 'rating', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+    rate: {type: DataTypes.INTEGER, allowNull: false},
+})
