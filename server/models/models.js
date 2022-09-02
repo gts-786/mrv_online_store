411e80ed65c37ce00adc0742fd33d58f9b447803
Roman Markovich <gts-786@mail.ru>
2022-09-02 17:26:26 +0300

    Finished models and add it in index.js

--- server/models/models.js
+++ server/models/models.js
@@ -44,3 +44,46 @@ const Rating = sequelize.define( 'rating', {
     id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
     rate: {type: DataTypes.INTEGER, allowNull: false},
 })
+
+const TypeBrand = sequelize.define( 'type_brand', {
+    id: {type: DataTypes.INTEGER, primaryKey: true, autoIncrement: true},
+})
+
+User.hasOne(Basket)
+Basket.belongsTo(User)
+
+User.hasMany(Rating)
+Rating.belongsTo(User)
+
+Basket.hasMany(BasketDevice)
+BasketDevice.belongsTo(Basket)
+
+BasketDevice.hasOne(Device)
+Device.belongsTo(Basket)
+
+Device.hasMany(DeviceInfo)
+DeviceInfo.belongsTo(Device)
+
+Type.hasMany(Device)
+Device.belongsTo(Type)
+
+Brand.hasMany(Device)
+Device.belongsTo(Brand)
+
+Device.hasMany(Rating)
+Rating.belongsTo(Device)
+
+Type.belongsToMany(Brand, {through: TypeBrand})
+Brand.belongsToMany(Type, {through: TypeBrand})
+
+module.exports = {
+    User,
+    Basket,
+    BasketDevice,
+    Device,
+    Type,
+    Brand,
+    Rating,
+    TypeBrand,
+    DeviceInfo
+}
