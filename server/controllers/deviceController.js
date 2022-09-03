26fd4251e0490e90886d96a6c79f942b463cb2a6
Roman Markovich <gts-786@mail.ru>
2022-09-03 11:44:18 +0300

    Add fileupload and uuid

--- server/controllers/deviceController.js
+++ server/controllers/deviceController.js
@@ -1,6 +1,10 @@
+const uuid = require('uuid')
+
 class DeviceController {
     async create (req, res){
-
+        const {name, price, brandId, typeId, info} = req.body
+        const {img} = req.files
+        let fileName = uuid.v4() + '.jpg'
     }
 
     async getAll(req, res){
