0433dbdf8d1f3ac5e6468002cdfb4db6ddbbb6f2
Roman Markovich <gts-786@mail.ru>
2022-09-03 10:20:54 +0300

    Add ApiError.js and same class inside

--- server/controllers/userController.js
+++ server/controllers/userController.js
@@ -2,13 +2,12 @@ class UserController {
     async registration(req, res){
 
     }
-
     async login(req, res){
 
     }
-
     async check(req, res){
-
+        const query = req.query
+        res.json(query)
     }
 }
 
